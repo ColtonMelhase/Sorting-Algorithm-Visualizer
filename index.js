1b06baf7b984082e313ece8becc4e39a1da99d54
Colton Melhase <coltonmelhase12@gmail.com>
2023-08-17 09:25:16 -0700

    Add render

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ function gen_random_array(arr) {
     }
 
     console.log(arr);
+    render(arr);
 }
+
+function render(arr) {
+
+    let stage = document.getElementById("stage");
+
+    stage.innerHTML = '';
+
+    for(let i = 0; i < arr.length; i++) {
+        let bar = document.createElement("div");
+        bar.classList.add("bar");
+        bar.style.height = `${arr[i]}px`
+        bar.style.width = `${2}px`
+
+        stage.appendChild(bar);
+    }
+}
+
+    
